167bda086566a0574a508edc86d0efba91ca0004
Caleb-Morgan <823691592@qq.com>
2018-09-02 00:24:20 +0800

    新增配置

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from 'routes/index'
+// import registerServiceWorker from './registerServiceWorker'
+
+ReactDOM.render(<App />, document.getElementById('root'))
