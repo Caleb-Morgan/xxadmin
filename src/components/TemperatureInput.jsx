de97c8dd9077b884a36ac566f096e236305e3d11
Caleb <823691592@qq.com>
2018-09-17 16:50:01 +0800

    修改test页面

--- src/components/TemperatureInput.jsx
+++ src/components/TemperatureInput.jsx
@@ -8,31 +8,17 @@ export default class TemperatureInput extends Component{
         }
     }
     handelChange(e){
-        this.setState({
+        this.props.onTemperatureChange(e.target.value);
+        /* this.setState({
             temperature: e.target.value
-        })
-    }
-    toCelsius = (e, fahrenheit) => {
-        return (fahrenheit-32) * 5 / 9;
-    }
-    toFahrenheit = (e, celsius) => {
-        return (celsius / 9 * 5) + 32;
-    }
-    tryConvert = (e, temperature, convert) => {
-        const input = parseFloat(temperature);
-        if(Number.isNaN(input)){
-            return '';
-        }
-        const output = convert(input);
-        const rounded = Math.round(output * 1000) / 1000
-        return rounded;
+        }) */
     }
     render(){
         const scaleName = {
             c: 'Celsius',
             f: 'Fahrenheit'
         }
-        const temperature = this.state.temperature;
+        const temperature = this.props.temperature;
         const scale = this.props.scale;
         return(
             <fieldset>
--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -3,11 +3,44 @@ import BoilingVerdict from './BoilingVerdict.jsx';
 import Temperatureinput from './TemperatureInput.jsx'
 
 export default class Test extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            temperature: '',
+            scale: 'c'
+        }
+    }
+    handelCelsiusChange(temperature){
+        this.setState({scale: 'c', temperature})
+    }
+    handelFahrenheitChange(temperature){
+        this.setState({scale: 'f', temperature})
+    }
+    toCelsius(fahrenheit){
+        return (fahrenheit-32) * 5 / 9;
+    }
+    toFahrenheit(celsius){
+        return (celsius / 9 * 5) + 32;
+    }
+    tryConvert(temperature, convert){
+        const input = parseFloat(temperature);
+        if(Number.isNaN(input)){
+            return '';
+        }
+        const output = convert(input);
+        const rounded = Math.round(output * 1000) / 1000
+        return rounded.toString();
+    }
     render(){
+        const scale = this.state.scale;
+        const temperature = this.state.temperature;
+        const celsius = scale === 'f' ? this.tryConvert(temperature,this.toCelsius) : temperature;
+        const fahrenheit = scale === 'c' ? this.tryConvert(temperature,this.toFahrenheit) : temperature;
         return(
             <div>
-                <Temperatureinput scale="c"/>
-                <Temperatureinput scale="f"/>
+                <Temperatureinput scale="c" temperature={celsius} onTemperatureChange={ this.handelCelsiusChange.bind(this) }/>
+                <Temperatureinput scale="f" temperature={fahrenheit} onTemperatureChange={ this.handelFahrenheitChange.bind(this) }/>
+                <BoilingVerdict celsius={parseFloat(celsius)} />
             </div>
         )
     }
